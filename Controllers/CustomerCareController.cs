c40aad45ae36b9e7407f84518ccdaeffdbc7c177
unknown <>
2017-02-06 17:56:40 -0500

    Ask Professor

--- Controllers/CustomerCareController.cs
+++ Controllers/CustomerCareController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+
+[Route("customer")]
 public class CustomerCareController : Controller
 {
     public IActionResult Index() 
@@ -6,10 +8,21 @@ public class CustomerCareController : Controller
         return Ok("This is the index of the CustomerCareController"); 
     }
 
-    public IActionResult Claims() 
+    [Route("claim")]
+    public IActionResult Quote() 
     {
-        return Ok("This is the claims of the CustomerCareController"); 
+        return Ok("This is the claim of the CustomerCareController"); 
+    }
+
+    [Route("claim/fileclaim")]
+    public IActionResult NewClaim()        
+    {
+        return Ok("This is the fileclaims of the CustomerCareController"); 
     }
     
+    [Route("claim/myclaims")]
+    public IActionResult ClaimHistory()        
+    {
+        return Ok("This is the myclaims of the CustomerCareController"); 
+    }
 }
- 
