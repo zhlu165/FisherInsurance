b1665b92df056f14c80f9a51335e5f9b33281ad2
unknown <>
2017-02-08 18:07:56 -0500

    Add models

---
+++ Models/QuoteModel.cs
@@ -0,0 +1,9 @@
+using System;
+namespace FisherInsurance.Models
+ 
+{
+    public class Quote
+    {
+public int Id { get; set; }
+public string Product { get; set; } public DateTime ExpireDate { get; set; } public decimal Price { get; set; }
+} }
